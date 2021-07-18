8c02031f433f09475a43c0bfa683418a63baea54
unknown <2stndard@gmail.com>
2021-07-18 23:53:20 +0900

    210718-2353

--- No1/movearea.R
+++ No1/movearea.R
@@ -1,6 +1,9 @@
 library(readxl)
 library(dplyr)
 library(tidyverse)
+library(showtext)
+showtext_auto()
+library("RColorBrewer")
 
 getwd()
 
@@ -41,25 +44,18 @@ df <- df %>% mutate(grad.year =
 ) %>%
   relocate(grad.year, .after = 졸업연도)
 
-
+df <- df |>
+  mutate(구분 = replace(구분, 구분 == '01 대학', '대학')) |>
+  mutate(구분 = replace(구분, 구분 == '08 전문대학', '전문대학'))
+  
 
 # df |>
 #   gather(입학시도, 입학자수, starts_with('입학')) |>
 #   filter(연도 == 2018, 입학시도 != '입학합계') |>
 #   mutate(입학시도1 = fct_recode(입학시도, '서울' = '입학서울', '부산' = '입학부산', '대구' = '입학대구', '인천' = '입학인천', '광주' = '입학광주','대전' = '입학대전','울산' = '입학울산','세종' = '입학세종', '경기' = '입학경기','강원' = '입학강원', '충북' = '입학충북', '충남' = '입학충남', '전북' = '입학전북', '전남' = '입학전남', '경북' = '입학경북', '경남' = '입학경남', '제주' = '입학제주')) |> View()
 
-df |>
-  gather(입학시도, 입학자수, 7:24) |>
-  filter(연도 == 2020, 입학시도 != '입학합계', 졸업연도 != '계', 대학시도 != '전국', 대학시도 == '서울') |>
-  mutate(입학시도 = fct_relevel(입학시도, '서울', '부산', '대구', '인천', '광주', '대전', '울산', '세종', '경기', '강원', '충북', '충남', '전북', '전남', '경북', '경남', '제주')) |>
-  group_by(구분, 입학시도) |>
-  summarise(입학자수 = sum(입학자수)) |>
-  ggplot(aes(x = 입학시도, y = 입학자수)) + 
-  geom_bar(stat="identity", fill=alpha("skyblue", 0.7)) + 
-  geom_text(aes(x = 입학시도, y = 입학자수, label = 입학자수), vjust = -0.5) + 
-  facet_wrap(~구분, nrow = 2) +
-  theme_minimal()
 
+##  전체 시도별 입학생의 출신 고교 분포
 df |>
   gather(입학시도, 입학자수, 7:24) |>
   filter(연도 == 2020, 입학시도 != '입학합계', 졸업연도 == '계') |>
@@ -68,33 +64,41 @@ df |>
   mutate(pct = 입학자수/합계) |>
   ggplot(aes(x = 대학시도, y = pct, fill = 입학시도)) + 
   geom_bar(position="fill", stat="identity") + 
-#  geom_text(aes(label = scales::percent(pct)),
-#              position="stack",hjust=0.5,col="firebrick",size=3) +
+  #  geom_text(aes(label = scales::percent(pct)),
+  #              position="stack",hjust=0.5,col="firebrick",size=3) +
   facet_wrap(~구분, nrow = 2) + 
-##  coord_flip() +
+  ##  coord_flip() +
+  labs(title = '시도별 (전문)대학 입학생 출신 고교 지역 분포', 
+       x = '대학 소재지', fill = '출신 고교 분포') + 
+##  scale_fill_brewer(palette = "Set1") + 
   theme_minimal()
 
 
+## Heat Map
 ## 학생수
+
+year <- 2019
+
 df |>
   gather(입학시도, 입학자수, 7:24) |>
-  filter(연도 == 2020, 입학시도 != '입학합계', 졸업연도 == '계', 대학시도 != '전국') |>
+  filter(연도 == year, 입학시도 != '입학합계', 졸업연도 == '계', 대학시도 != '전국') |>
   mutate(입학시도 = fct_relevel(입학시도, '서울', '부산', '대구', '인천', '광주', '대전', '울산', '세종', '경기', '강원', '충북', '충남', '전북', '전남', '경북', '경남', '제주')) |>
   group_by(구분, 대학시도, 입학시도) |>
   mutate(pct = 입학자수/합계) |>
   ggplot(aes(x = 대학시도, y = 입학시도, fill = pct)) + 
   geom_tile() +
   geom_text(aes(label = scales::number(입학자수, big.mark = ',', accuracy = 1))) +
-  facet_wrap(~구분, nrow = 2) + 
+  facet_wrap(~구분, nrow = 2, scales = 'free') + 
   scale_fill_gradient(low="white", high="darkblue") + 
-  labs(y = '고교졸업생시도', x = '대학소재지시도')
+  labs(title = paste0(year, '년도 ', '지역별 (전문)대학 신입생 고교 출신지역 분포'),
+       y = '출신고교 소재지역', x = '대학소재지') 
 
 
 
 ## 대학시도 100% 기준
 df |>
   gather(입학시도, 입학자수, 7:24) |>
-  filter(연도 == 2020, 입학시도 != '입학합계', 졸업연도 == '계', 대학시도 != '전국') |>
+  filter(연도 == year, 입학시도 != '입학합계', 졸업연도 == '계', 대학시도 != '전국') |>
   mutate(입학시도 = fct_relevel(입학시도, '서울', '부산', '대구', '인천', '광주', '대전', '울산', '세종', '경기', '강원', '충북', '충남', '전북', '전남', '경북', '경남', '제주')) |>
   group_by(구분, 대학시도, 입학시도) |>
   mutate(pct = 입학자수/합계) |>
@@ -103,7 +107,8 @@ df |>
   geom_text(aes(label = scales::percent(round(pct, 3), accuracy=0.1))) +
   facet_wrap(~구분, nrow = 2) + 
   scale_fill_gradient(low="white", high="darkblue") + 
-  labs(y = '고교졸업생시도', x = '대학소재지시도')
+  labs(title = paste0(year, '년도 ', '지역별 (전문)대학 신입생 고교 출신지역 분포 비율'),
+       y = '출신고교 소재지역', x = '대학소재지') 
 
 
   
@@ -120,5 +125,39 @@ df |>
   geom_text(aes(label = scales::percent(round(pct, 4), accuracy=0.01))) +
   facet_wrap(~구분, nrow = 2) + 
   scale_fill_gradient(low="white", high="darkblue") + 
-  labs(x = '고교졸업생시도', y = '대학소재지시도')
+  labs(title = paste0(year, '년도 ', '출신 고교 지역별 (전문)대학 입학지역 분포 비율'),
+       x = '출신고교 소재지역', y = '대학소재지')
 
+
+
+## Bar chart
+## 소재 (전문)대학 신입생 고교 출신지역 분포
+
+대학소재지 <- '대구'
+
+df |>
+  gather(입학시도, 입학자수, 7:24) |>
+  filter(연도 == year, 입학시도 != '입학합계', 졸업연도 != '계', 대학시도 != '전국', 대학시도 == 대학소재지) |>
+  mutate(입학시도 = fct_relevel(입학시도, '서울', '부산', '대구', '인천', '광주', '대전', '울산', '세종', '경기', '강원', '충북', '충남', '전북', '전남', '경북', '경남', '제주')) |>
+  group_by(구분, 입학시도) |>
+  summarise(입학자수 = sum(입학자수)) |>
+  ggplot(aes(x = 입학시도, y = 입학자수)) + 
+  geom_bar(stat="identity", fill=alpha("skyblue", 0.7)) + 
+  geom_text(aes(x = 입학시도, y = 입학자수, label = format(입학자수, big.mark = ',')), vjust = -0.5) + 
+  facet_wrap(~구분, nrow = 2, scales = "free") +
+  labs(title = paste0(year, '년도 ', 대학소재지, '소재 (전문)대학 신입생 고교 출신지역 분포'), 
+       x = '출신교고 소재지역', 
+       y = '입학자수') +
+  theme_minimal()
+
+
+
+df |>
+  gather(입학시도, 입학자수, 6:24) |> 
+  filter(연도 == 2020, 입학시도 != '합계', 졸업연도 == '계', 대학시도 != '전국') |>
+  mutate(입학시도 = fct_relevel(입학시도, '서울', '부산', '대구', '인천', '광주', '대전', '울산', '세종', '경기', '강원', '충북', '충남', '전북', '전남', '경북', '경남', '제주')) |>
+  group_by(연도, 구분, 입학시도) |>
+  mutate(입학생합계 = sum(입학자수)) |> 
+  mutate(pct = 입학자수/입학생합계) |>
+  ggplot(aes(x = 대학시도, y = 입학자수)) + 
+  geom_bar(stat="identity", fill=alpha("skyblue", 0.7))
